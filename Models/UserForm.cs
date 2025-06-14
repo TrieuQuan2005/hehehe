1c93feb90a2f93f241a1903f6e97e5a0950e0ca1
Trieu Quoc Quan <trieuquanno1@trieuquan.com>
2025-06-14 19:26:49 +0700

    based

---
+++ Models/UserForm.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hehehe.Models
+{
+    public class StudentForm
+    {
+        [Key]
+        [ForeignKey("User")]
+        public string MaNhapHoc { get; set; } = string.Empty;
+
+        public string HoTen { get; set; } = string.Empty;
+
+        public DateTime NgaySinh { get; set; }
+
+        public float DiemThi { get; set; }
+
+        public string? UploadedFilePath { get; set; }
+
+        public bool IsLocked { get; set; } = false;
+
+        public User? User { get; set; }
+    }
+}
